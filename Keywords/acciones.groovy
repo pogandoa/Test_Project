3101a027b7aba26ce557f7d753bacbac2f2088ed
Pedro Ogando Aquino <pogando@aginc.local>
2020-05-22 17:01:49 -0400

    Incluyendo estructura de construccion Framework

--- Keywords/acciones.groovy
+++ Keywords/acciones.groovy
@@ -3,6 +3,8 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import java.util.concurrent.TimeUnit
+
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory
@@ -22,7 +24,7 @@ import internal.GlobalVariable
 import MobileBuiltInKeywords as Mobile
 import WSBuiltInKeywords as WS
 import WebUiBuiltInKeywords as WebUI
-
+import groovyjarjarasm.asm.tree.TryCatchBlockNode
 
 import org.openqa.selenium.chrome.*
 import org.openqa.selenium.WebElement
@@ -36,7 +38,7 @@ import com.kms.katalon.core.testobject.RequestObject
 import com.kms.katalon.core.testobject.ResponseObject
 import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObjectProperty
-
+import com.kms.katalon.core.trymonad.Try
 import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
 import com.kms.katalon.core.util.KeywordUtil
 
@@ -44,7 +46,7 @@ import com.kms.katalon.core.webui.exception.WebElementNotFoundException
 
 
 class acciones {
-	WebDriver driver = new ChromeDriver()
+	
 	
 	/**
 	 * Refresh browser
@@ -55,6 +57,7 @@ class acciones {
 		WebDriver webDriver = DriverFactory.getWebDriver()
 		webDriver.navigate().refresh()
 		KeywordUtil.markPassed("Refresh successfully")
+		webDriver.findElement(null)
 	}
 
 	/**
@@ -77,25 +80,98 @@ class acciones {
 	
 	
 	/**
-	 * Click element
-	 * @param to Katalon test object
+	 * Click en un element usando XPath selector
+	 * @param WebDriver
+	 * @param String
+	 * @author Pedro Ogando
 	 */
 	@Keyword
 	def darClick(String xpath) {
 		
-		try {
-			WebElement element = driver.findElement(By.xpath(xpath))
-			KeywordUtil.logInfo("Clicking element")
-			element.click()
-			KeywordUtil.markPassed("Element has been clicked")
+		try {			
+			WebDriver driver = DriverFactory.getWebDriver()
+			KeywordUtil.logInfo("Clicking element")		
+			driver.findElement(By.xpath(xpath)).click()		
+			KeywordUtil.markPassed("Se ha hecho clic en el elemento.")
 		} catch (WebElementNotFoundException e) {
-			KeywordUtil.markFailed("Element not found")
+			KeywordUtil.markFailed("Elemento no encontrado")
 		} catch (Exception e) {
-			KeywordUtil.markFailed("Fail to click on element")
+			KeywordUtil.markFailed("Error al hacer clic en el elemento")
+		}
+	}
+	
+	/**
+	 * Click en un elemento Link usando XPath Selector
+	 * @param WebDriver
+	 * @param String
+	 * @author Pedro Ogando
+	 * 
+	 **/
+	@Keyword
+	def darClickLink(String linkText){
+		try{
+			WebDriver driver = DriverFactory.getWebDriver()
+			driver.findElement(By.xpath("//a[contains(text(),'"+ linkText +"')]")).click()
+			KeywordUtil.markPassed("Se ha hecho clic en el elemento.")
+		}catch(WebElementNotFoundException e){
+			KeywordUtil.markFailed("Elemento no encontrado")
+		}catch(Exception e){
+			KeywordUtil.markFailed("Error al hacer clic en el elemento")
 		}
 	}
 	
 
+	/**
+	 * Realiza una espera
+	 * @param String
+	 * @param WebDriver
+	 * @author Pedro Ogando
+	 * **/
+	@Keyword
+	def espera(Integer seg){
+		WebDriver driver = DriverFactory.getWebDriver()
+		driver.manage().timeouts().implicitlyWait(seg, TimeUnit.SECONDS)
+	}	
+	
+	/**
+	 * Selecciona un elemento de una lista desplegable
+	 * @param WebDriver
+	 * @param String
+	 * @author Pedro Ogando
+	 * **/
+	@Keyword
+	def selectItem(String nameItem){
+		try{
+			WebDriver driver = DriverFactory.getWebDriver()
+			driver.findElement(By.xpath("//option[contains(text(),'"+ nameItem +"')]")).click()
+			KeywordUtil.markPassed("Se ha seleccionado en el elemento.")
+		}catch(WebElementNotFoundException e){
+			KeywordUtil.markFailed("Elemento no encontrado")
+		}catch(Exception e){
+			KeywordUtil.markFailed("Error al seleccionar el elemento")
+		}
+	}
+	
+	/**
+	 * Introduce un texto en un TextBox
+	 * @param WebDriver
+	 * @param String
+	 * @author Pedro Ogando
+	 * **/	
+	@Keyword
+	def ponerTexto(String xpath, String text){
+		try{
+			WebDriver driver = DriverFactory.getWebDriver()
+			driver.findElement(By.xpath(xpath)).sendKeys(text)
+			KeywordUtil.markPassed("Se ha colocado el texto en el elemento.")
+		}catch(WebElementNotFoundException e){
+			KeywordUtil.markFailed("Elemento no encontrado")
+		}catch(Exception){
+			KeywordUtil.markFailed("Error al colocado el texto en el elemento")
+		}
+	}
+
+	
 	/**
 	 * Get all rows of HTML table
 	 * @param table Katalon test object represent for HTML table
--- Scripts/Iniciar Sesion/Script1590145889833.groovy
+++ Scripts/Iniciar Sesion/Script1590145889833.groovy
@@ -3,6 +3,11 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import java.util.concurrent.TimeUnit as TimeUnit
+import org.openqa.selenium.*
+//import org.openqa.selenium.chrome
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -17,7 +22,18 @@ import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser(GlobalVariable.Url)
 
-WebUI.waitForPageLoad(120)
+//WebUI.navigateToUrl("http://practice.automationtesting.in/")
+CustomKeywords.'acciones.espera'(30)
 
-CustomKeywords.'acciones.darClick'("//li[@id='menu-item-50']/a")
+CustomKeywords.'acciones.darClick'('//li[@id="menu-item-50"]/a')
+
+CustomKeywords.'acciones.espera'(15)
+
+//CustomKeywords.'acciones.selectItem'('Sort by price: high to low')
+CustomKeywords.'acciones.ponerTexto'("//input[@id='username']", GlobalVariable.Username)
+CustomKeywords.'acciones.ponerTexto'("//input[@id='password']",GlobalVariable.Password)
+
+CustomKeywords.'acciones.darClick'("//input[@type='submit']")
+
+
 
