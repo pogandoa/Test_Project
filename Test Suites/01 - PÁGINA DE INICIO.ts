bcabc7541115ceab916d53b845c356e786ee3227
Pedro Ogando Aquino <pogando@aginc.local>
2020-05-22 10:49:52 -0400

    incluyendo caso

---
+++ Test Suites/01 - PÁGINA DE INICIO.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>01 - PÁGINA DE INICIO</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>701fc5e0-9ac4-4cbf-9232-dad0b9e8139a</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/03 - TIENDA.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>03 - TIENDA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>dbc0ac3a-898d-4cf7-bc69-0adfe5297c6b</testSuiteGuid>
+</TestSuiteEntity>
